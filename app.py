8e9df91542c137d5394aba2768ec4404bb8a582d
Naresh <nareshm1996@users.noreply.github.com>
2020-04-02 15:22:39 +0530

    First Commit

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Hello AdRoll"
 
+@app.route('/<string:name>')
+def index(name):
+    return "Hello, " + name
 
 if __name__ == "__main__":
     app.run(debug='true')
